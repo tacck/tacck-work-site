bb63211af1977f7a0f2aff2a637a4bcdedc738c6
Kihara, Takuya <gray@tacck.net>
2022-08-11 13:19:09 +0900

    change title tags

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,53 +1,53 @@
 import React from "react";
 
-import { Typography, Stack } from "@mui/material";
+import { Typography, Box, Stack } from "@mui/material";
 import { grey } from '@mui/material/colors';
 import {
-  createTheme,
-  ThemeProvider as MuiThemeProvider,
+  // createTheme,
+  // ThemeProvider as MuiThemeProvider,
   styled
 } from "@mui/material/styles";
 
 // import TacckFont from '../../../static/fonts/tacckfont.woff2';
 import "./TacckFont.css";
 
-const TacckFontTheme = createTheme({
-  // typography: {
-  //   fontFamily: 'TacckFont',
-  // },
-  // components: {
-  //   MuiCssBaseline: {
-  //     styleOverrides: `
-  //       @font-face {
-  //         font-family: 'TacckFont';
-  //         font-style: normal;
-  //         font-display: swap;
-  //         src: url("../../../static/fonts/tacckfont.ttf") format('ttf');
-  //       }
-  //     `,
-  //   },
-  // },
-  palette: {
-    primary: {
-      main: "#4B5F65",
-    },
-    secondary: {
-      main: "#CECCB8",
-    },
-    success: {
-      main: "#B7C6C7",
-    },
-    info: {
-      main: "#E3E6E1",
-    },
-    warning: {
-      main: "#C7B5A8",
-    },
-    error: {
-      main: "#918879",
-    },
-  },
-});
+// const TacckFontTheme = createTheme({
+//   // typography: {
+//   //   fontFamily: 'TacckFont',
+//   // },
+//   // components: {
+//   //   MuiCssBaseline: {
+//   //     styleOverrides: `
+//   //       @font-face {
+//   //         font-family: 'TacckFont';
+//   //         font-style: normal;
+//   //         font-display: swap;
+//   //         src: url("../../../static/fonts/tacckfont.ttf") format('ttf');
+//   //       }
+//   //     `,
+//   //   },
+//   // },
+//   palette: {
+//     primary: {
+//       main: "#4B5F65",
+//     },
+//     secondary: {
+//       main: "#CECCB8",
+//     },
+//     success: {
+//       main: "#B7C6C7",
+//     },
+//     info: {
+//       main: "#E3E6E1",
+//     },
+//     warning: {
+//       main: "#C7B5A8",
+//     },
+//     error: {
+//       main: "#918879",
+//     },
+//   },
+// });
 
 const TitleStyle = styled('div')(
   ({theme}) => ({
@@ -75,13 +75,30 @@ const Title = (): JSX.Element => (
     sx={{
       color: grey[100],
       bgcolor: "primary.main",
+      lineHeight: '2ex',
+          textAlign: 'center',
+          fontSize: {
+            xs: "2.5rem",
+            sm: "3rem",
+            md: "5rem",
+            lg: "6rem",
+          }
     }}
+    className="tacck-font"
   >
-    <Stack justifyContent="center" alignItems="center">
-      <MuiThemeProvider theme={TacckFontTheme}>
-        <TitleStyle className="tacck-font">Tacck Work</TitleStyle>
-      </MuiThemeProvider>
-    </Stack>
+    Tacck Work
+    {/* <Stack justifyContent="center" alignItems="center">
+      <Box className="tacck-font" sx={{
+          lineHeight: '2ex',
+          textAlign: 'center',
+          fontSize: {
+            xs: "2.5rem",
+            sm: "3rem",
+            md: "5rem",
+            lg: "6rem",
+          }
+        }}>Tacck Work</Box>
+    </Stack> */}
   </Stack>
 );
 
